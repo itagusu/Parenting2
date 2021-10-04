ba9360edbec2c61048e95d343c559633aa5289b1
itagusu <ir060906@gmail.com>
2021-10-04 14:02:30 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'public/homes#top'
+  get 'serch' => 'serches#serch'
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    get 'users/confirm' => 'users#confirm'
+    patch 'users/withdraw' => 'users#withdraw'
+    resources :users, only: [:edit, :update, :index, :show] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+    resources :posts, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+  end
+
+  namespace :admin do
+    resources :users, only: [:edit, :update, :index, :show]
+    resources :genres, only: [:create, :edit, :update, :index, :destroy]
+  end
 
 end
 
