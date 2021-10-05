04d091b5292c0719cb0a0bf80b6158b93bb1b9b2
itagusu <ir060906@gmail.com>
2021-10-05 14:19:42 +0000

    adminユーザーページ作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,28 @@
 class Admin::UsersController < ApplicationController
+
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :email, :introduction, :is_deleted)
+  end
+
 end
+
