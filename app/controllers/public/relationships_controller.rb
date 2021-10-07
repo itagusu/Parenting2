0c064958eb7dd576535b10b02080a095ae7252e1
itagusu <ir060906@gmail.com>
2021-10-07 15:04:17 +0000

    フォロー機能作成

---
+++ app/controllers/public/relationships_controller.rb
@@ -0,0 +1,26 @@
+class Public::RelationshipsController < ApplicationController
+  # ログインしてなければフォローできなくする
+  before_action :authenticate_user!
+
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+    # request.referer 遷移前のページにリダイレクト
+  end
+
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @usres = user.follwings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_023441) do
+ActiveRecord::Schema.define(version: 2021_10_07_074251) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2021_10_07_023441) do
     t.integer "jenre_id"
     t.string "image_id"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
