73fdd0f74a5c6802ccc96a0723beb910e1789366
itagusu <ir060906@gmail.com>
2021-10-13 10:20:22 +0000

    レイアウト調整

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,11 +2,13 @@ class Public::SearchesController < ApplicationController
 
   def search
     @posts = Post.looks(params[:word])
+    @genres = Genre.all
     #@users = User.looks(params[:word])
   end
 
   def genre
     @genre = Genre.find(params[:id])
     @posts = @genre.posts
+    @genres = Genre.all
   end
 end
