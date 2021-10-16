e0b1bfe835bb56038b11df9448551b9512aae2b9
itagusu <ir060906@gmail.com>
2021-10-16 12:05:48 +0000

    通知機能作成途中

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -3,6 +3,7 @@ class Public::NotificationsController < ApplicationController
     @notifications = current_user.passive_notifications
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
+      
     end
   end
 end
