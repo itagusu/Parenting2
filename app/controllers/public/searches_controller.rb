d819594ec9defe59076695c444103ad64ff9692c
itagusu <ir060906@gmail.com>
2021-10-23 03:33:50 +0000

    レイアウト調整

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,7 +2,6 @@ class Public::SearchesController < ApplicationController
   def search
     @posts = Post.looks(params[:word]).page(params[:page]).per(7).order(created_at: :desc)
     @genres = Genre.all
-    #@users = User.looks(params[:word])
   end
 
   def genre
