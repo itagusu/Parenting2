32a755ed8ded7504c56315fb6f32f9a9f18fadb5
itagusu <ir060906@gmail.com>
2021-11-04 03:57:10 +0000

    細かい修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,6 @@
 class Public::PostsController < ApplicationController
   before_action :set_genres, only: %i[new create show index destroy]
+  before_action :authenticate_user!,except: [:index]
 
   def new
     @post = Post.new
